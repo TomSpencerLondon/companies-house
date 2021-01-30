22ebd5dc8fda2ca6404263a9c6b1b7e1155e5c46
Tom <thomas.spencer@codurance.com>
2021-01-30 19:09:14 +0000

    Delete comments

--- main.py
+++ main.py
@@ -28,8 +28,5 @@ def main():
                 companies.append(company['properties']['name'])
     print(companies)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
